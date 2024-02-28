7699182ad7f9310601828f679b6e9aa0ea67ce0b
EmilianaManzo <emiliana.ma96@gmail.com>
2024-02-28 19:34:13 +0100

    [add] pari e dispari

--- README.md
+++ README.md
@@ -10,3 +10,17 @@ Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzion
 Sommiamo i due numeri
 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.
+
+## Svolgimento di Palindromo
+1. creo un input dove chiedo la parola all'utente
+2. creo la mia function 
+3. all'interno della function creo una variabile che mi divide la parola inserita in due metà
+4. creo un ciclo for stabilisco se è la parola è palindroma o no
+
+## Svolgimento di Pari e Dispari 
+1. tramite un select faccio scegliere all'utente tra pari e dispari
+2. tramite un input faccio inserire all'utente un numero tra 1 e 5 
+3. creo un tasto che genera tutto
+4. genero un numero random per il pc sempre tra 1 e 5
+5. sommo i due numeri 
+6. con una function decreto se il numero della somma è pari o dispari e proclamo il vincitore . 
--- javascript/script.js
+++ javascript/script.js
@@ -2,22 +2,84 @@
 
 // Trovato su stackoverflow - Principio Kiss  
 
-const word = prompt('Inserisci una parola');
+const wordinput = document.getElementById('insertword'); 
+const btn = document.getElementById('btn');
 
 
-function palindrome(string){
-  let lunghezzaParola = Math.floor(word.length / 2);
-  for (let i = 0; i < lunghezzaParola; i++){
-    if (word[i] !== word[word.length - i - 1]){
+btn.addEventListener('click', function(){
+  const word = wordinput.value;  
+  palindrome(word);
+})
+
+
+
+function palindrome(parola){
+  let lunghezzaParola = Math.floor(parola.length / 2); // Divido la parola in due metà
+
+  debugger;
+  for (let i = 0; i < lunghezzaParola; i++){   
+    if (parola[i] !== parola[parola.length - 1 -i]){  //Finché i caratteri di ciascuna parte corrispondono, il ciclo FOR continuerà
       document.getElementById('palindromo').innerHTML = 'La parola non è palindroma';
-      return false;
+      return false; //Quando i caratteri non corrispondono più, viene restituito false e usciamo dal ciclo FOR
     }else{
       document.getElementById('palindromo').innerHTML = 'La parola è palindroma';
-      return true;
+      return true; // Entrambe le parti sono rigorosamente uguali quindi è true e la parola è palindroma
     }
   }   
 }
 
 
-palindrome(word);
-console.log(palindrome(word));
+
+
+
+//PARI E DISPARI
+const pariDispari = document.getElementById('paridispari');
+const insertNumber = document.getElementById('insertnumber');
+const spin = document.getElementById('spin');
+
+console.log(pariDispari, insertNumber, spin);
+
+spin.addEventListener('click',function(){
+  const numberValue= parseInt(insertNumber.value);
+  const pariDispariValue = pariDispari.value
+
+    
+  if((numberValue < 1) || (numberValue > 5)){
+    alert('il numero inserito non è valido');
+  }else{
+    const esito = document.getElementById('esito');
+    const pc = numberopc();
+    const somma = numberValue + pc ;
+    console.log(somma,numberValue, pc);
+
+    document.getElementById('numberpc').innerHTML = pc;
+    document.getElementById('sum').innerHTML = somma;
+
+    pd(somma,pariDispariValue,esito);
+  }
+
+})
+
+
+
+
+function numberopc(){
+  const numberRandom = Math.ceil(Math.random() * 5);
+  return numberRandom;
+
+}
+
+function pd(somma, pdv, ex ) {
+  if(somma % 2 === 0 && pdv === 'pari'){
+    ex.innerHTML = 'Vince il giocatore'
+  }else if(somma % 2 !== 0 && pdv === 'dispari'){
+    ex.innerHTML = 'Vince il giocatore'
+  }else{
+    ex.innerHTML = 'Vince il pc'
+  }
+}
+
+
+
+
+
